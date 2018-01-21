ad994f8416c0ff499658c39f04366956bd58a123
Simple zia <ziaulqamarzafar@yahoo.com>
2018-01-21 23:00:01 +0500

    conflict resoled

--- db/migrate/20180120154825_change_column_datatype.rb
+++ db/migrate/20180120154825_change_column_datatype.rb
@@ -1,6 +1,6 @@
 class ChangeColumnDatatype < ActiveRecord::Migration[5.1]
   def change
-    change_column :hotels, :start_time, :time
-    change_column :hotels, :end_time, :time
+    change_column :hotels, :start_time, :timestamp
+    change_column :hotels, :end_time, :timestamp
   end
 end
